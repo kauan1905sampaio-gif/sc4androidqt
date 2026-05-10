b626e7b4b685742ca43f34cc960ed22ceaabfab9
kauan1905sampaio-gif <kauan1905sampaio@gmail.com>
2026-05-10 00:23:32 -0300

    Delete SCEditorQt directory

--- SCEditorQt/src/Decompressor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ScFile.h"
-#include <QByteArray>
-#include <optional>
-
-class Decompressor {
-public:
-    static std::optional<QByteArray> decompress(const QByteArray &data, Compression c);
-};
